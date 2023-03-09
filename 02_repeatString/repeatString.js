d10b87395a1071ec77f045b5e6d05136be43d1f1
ryanwilliamdev <rywilldav@gmail.com>
2023-03-09 08:03:22 -0500

    npm test passed on 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let inputString = '';
+    for (i = 0; i < num; i++) {
+        inputString += str;
+    }
+    return inputString;
 };
-
+repeatString('hey', 3);
+// repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
