697c8ab51207cad5b6939c89ad170499d95e8c17
ryanwilliamdev <rywilldav@gmail.com>
2023-03-10 07:48:59 -0500

    npm tests passed on 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(str) {
+    const arrayStrings = str.split("");
+    const reverseArray = arrayStrings.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;
+}
+// const inputString = prompt("Enter a string: ");
+// const result = reverseString(inputString);
+// alert(result);
 // Do not edit below this line
 module.exports = reverseString;
